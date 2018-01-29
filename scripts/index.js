c6389aeadd5125faefb9adc6da294ca4468b5ae6
Filip <landedcosine@live.se>
2018-01-29 19:42:02 +0100

    day 1

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+let changeColor = document.getElementById("knapp1");
+let paragraph = document.getElementById("p1");
+
+
+changeColor.addEventListener("click", function(){
+    paragraph.setAttribute("style","font-size: 2em")
+}
+)
---
+++ scripts/ovning2.js
@@ -0,0 +1,46 @@
+let btn1 = document.getElementById("btn1");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+
+btn1.addEventListener("click", function(){
+
+  console.log(input1.value, input2.value);
+    }
+)
+
+
+let link = document.getElementById("link1")
+let btn2 = document.getElementById("btn2")
+let hreflang = link1.getAttribute("hreflang")
+let target = link1.getAttribute("target")
+let href = link1.getAttribute("href")
+
+
+btn2.addEventListener("click", function(){
+
+        console.log(link.id);
+        console.log(hreflang);
+        console.log(target);
+        console.log(href);
+    }
+)
+
+
+
+let paragraph1 = document.createElement("h1");
+let text1 = document.createTextNode("My paragraph text");
+paragraph1.appendChild(text1);
+let existingElement1 = document.getElementsByTagName("div")[0];
+existingElement1.appendChild(paragraph1);
+
+let paragraph = document.createElement("p");
+let text = document.createTextNode("My paragraph text");
+paragraph.appendChild(text);
+let existingElement = document.getElementsByTagName("div")[0];
+existingElement.appendChild(paragraph);
+
+let x = document.createElement("IMG");
+x.setAttribute("width", "300");
+x.setAttribute("height", "200");
+x.setAttribute("src", "Tree.jpg");
+document.body.appendChild(x);
