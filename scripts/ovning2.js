df4fb04090a301cd9088edaa30e5e06afae109b5
Filip <landedcosine@live.se>
2018-01-31 12:57:21 +0100

    dag 2

--- scripts/ovning2.js
+++ scripts/ovning2.js
@@ -1,3 +1,6 @@
+
+/*
+
 let btn1 = document.getElementById("btn1");
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
@@ -44,3 +47,74 @@ x.setAttribute("width", "300");
 x.setAttribute("height", "200");
 x.setAttribute("src", "Tree.jpg");
 document.body.appendChild(x);
+
+
+
+let existingtable = document.getElementById("table")
+let addrow = document.getElementById("btn3")
+let delrow = document.getElementById("btn4")
+
+
+
+
+btn3.addEventListener("click", function () {
+
+
+    let row = existingtable.insertRow(0);
+    let rowcellett = row.insertCell(0);
+    let rowcelltwo = row.insertCell(1);
+    let rowcellthree = row.insertCell(2);
+
+    rowcellett.innerHTML = "123";
+    rowcelltwo.innerHTML = "123";
+    rowcellthree.innerHTML = "123";
+
+
+
+})
+
+btn4.addEventListener("click", function () {
+
+
+    existingtable.deleteRow(0)
+
+})
+
+
+
+
+
+
+window.onresize = windowy;
+
+
+function windowy() {
+
+    console.log("height", window.innerHeight, "width", window.innerWidth);
+
+}
+
+*/
+
+
+
+let input = document.getElementById("doe");
+let value = input.getAttribute("value")
+
+
+doe.addEventListener("keyup", function () {
+    console.clear();
+    console.log(doe.value);
+    console.log(value);
+})
+
+
+
+
+
+
+
+
+
+
+
